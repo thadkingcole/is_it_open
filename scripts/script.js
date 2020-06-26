92e876c7c2dbe18283b5ffdf64353b746506f4c2
Thad Cole <thadjcole@gmail.com>
2020-06-25 20:41:05 -0400

    making yelp api work with variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
-var myurl =
-  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-chloe&location=boston";
+const locationName = "Raleigh"; // taken from search form
+const categories = "restaurants,bars,parks";
+const bars = "bars";
+const restaurants = "restaurants";
+const parks = "active";
+
+// construct the initial search team 
+const myurl =
+  `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${locationName}&categories=${bars + restaurants + parks}`;
 
 $.ajax({
   url: myurl,
@@ -12,7 +19,7 @@ $.ajax({
   success: function (data) {
     console.log(data);
     // Grab the results from the API JSON return
-    var totalresults = data.total;
+    const totalresults = data.total;
     // If our results are greater than 0, continue
     if (totalresults > 0) {
       // Display a header on the page with the number of results
@@ -22,17 +29,17 @@ $.ajax({
       // Itirate through the JSON array of 'businesses' which was returned by the API
       $.each(data.businesses, function (i, item) {
         // Store each business's object in a variable
-        var id = item.id;
-        var alias = item.alias;
-        var phone = item.display_phone;
-        var image = item.image_url;
-        var name = item.name;
-        var rating = item.rating;
-        var reviewcount = item.review_count;
-        var address = item.location.address1;
-        var city = item.location.city;
-        var state = item.location.state;
-        var zipcode = item.location.zip_code;
+        const id = item.id;
+        const alias = item.alias;
+        const phone = item.display_phone;
+        const image = item.image_url;
+        const name = item.name;
+        const rating = item.rating;
+        const reviewcount = item.review_count;
+        const address = item.location.address1;
+        const city = item.location.city;
+        const state = item.location.state;
+        const zipcode = item.location.zip_code;
         // Append our result into our page
         $("#results").append(
           '<div id="' +
